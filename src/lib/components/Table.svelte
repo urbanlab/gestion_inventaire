b5d8bbfdf31f397e0175a33ca34951054d6f11cc
Yassin <yassin@siouda.com>
2023-10-10 12:27:10 +0200

    fix: table styling

--- src/lib/components/Table.svelte
+++ src/lib/components/Table.svelte
@@ -1,54 +1,61 @@
 <script lang="ts">
   import { InputChip } from "@skeletonlabs/skeleton";
 
-	export let items: any;
+  export let items: any;
 
-	function searchDesc(e) {
-		items.data = JSON.parse(window.localStorage.getItem('items')).data;
-		const input = e.target.value;
-		console.log('searchDesc');
+  function searchDesc(e) {
+    items.data = JSON.parse(window.localStorage.getItem("items")).data;
+    const input = e.target.value;
+    console.log("searchDesc");
 
-		// filter items.data.description with input
-		items.data = items.data.filter((item) => item?.descriptif?.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase().includes(input.toLowerCase()));
-	}
+    // filter items.data.description with input
+    items.data = items.data.filter((item) =>
+      item?.descriptif
+        ?.normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .toLowerCase()
+        .includes(input.toLowerCase())
+    );
+  }
 </script>
 
-<section class="overflow-y-scroll ">
-	{#if items?.data}
-        <div class="card rounded-b-none fixed w-full p-5">
-            <h1 class="mb-4">Recherche</h1>
-			<InputChip
-				name="search"
-				on:input={(e) => {
-					searchDesc(e);
-				}}
-			/>
-        </div>
-		<div class="table-container w-screen">
-			<table class="table table-hover table-compact mt-40 ">
-				<thead>
-					<tr>
-						<th>desc</th>
-						<th>projet</th>
-						<th>utilisation</th>
-						<th>quantité totale</th>
-						<th>infos utiles</th>
-					</tr>
-				</thead>
-				<tbody>
-					{#each items.data.slice(0, 100) as item}
-						<tr>
-							<td>{item.descriptif}</td>
-							<td>{item.projet}</td>
-							<td>{item.utilisation}</td>
-							<td>{item.quantite_totale}</td>
-							<td>{item.infos_utiles}</td>
-						</tr>
-					{/each}
-				</tbody>
-			</table>
-		</div>
-	{:else}
-		<p>loading...</p>
-	{/if}
+<section class="overflow-y-scroll">
+  {#if items?.data}
+    <div class="card rounded-t-none rounded-b-none fixed w-full p-5">
+      <h1 class="mb-7">Recherche</h1>
+      <InputChip
+        name="search"
+        on:input={(e) => {
+          searchDesc(e);
+        }}
+      />
+    </div>
+    <div class="table-container w-screen">
+      <table class="table table-hover table-compact mt-36">
+        <thead class="fixed h-20 w-full">
+          <tr>
+            <th>desc</th>
+            <th>projet</th>
+            <th>utilisation</th>
+            <th>quantité totale</th>
+            <th>infos utiles</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="h-24" />
+          {#each items.data.slice(0, 100) as item}
+            <tr>
+              <td>{item.descriptif}</td>
+              <td>{item.projet}</td>
+              <td>{item.utilisation}</td>
+              <td>{item.quantite_totale}</td>
+              <td>{item.infos_utiles}</td>
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+    </div>
+  {:else}
+    <p>loading...</p>
+  {/if}
 </section>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,140 +1,151 @@
 <script>
-  	import { json } from '@sveltejs/kit';
-	import { onMount } from 'svelte';
-	import { env } from '$env/dynamic/public';
-	// Your selected Skeleton theme:
-	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+  import { json } from "@sveltejs/kit";
+  import { onMount } from "svelte";
+  import { env } from "$env/dynamic/public";
+  // Your selected Skeleton theme:
+  import "@skeletonlabs/skeleton/themes/theme-skeleton.css";
 
-	// This contains the bulk of Skeletons required styles:
-	import '@skeletonlabs/skeleton/styles/all.css';
-	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
-    import { LightSwitch } from '@skeletonlabs/skeleton';
+  // This contains the bulk of Skeletons required styles:
+  import "@skeletonlabs/skeleton/styles/all.css";
+  import "@skeletonlabs/skeleton/themes/theme-crimson.css";
+  import { LightSwitch } from "@skeletonlabs/skeleton";
 
-	import '../app.css';
-	import Nav from '$lib/components/Nav.svelte';
-  	import { getAuth, logged, login, logout, notification } from '$lib/store';
-	import { ProgressBar } from '@skeletonlabs/skeleton';
-	import Fa from 'svelte-fa'
-	import { faCircle } from '@fortawesome/free-solid-svg-icons'
-  import Notification from '$lib/components/Notification.svelte';
+  import "../app.css";
+  import Nav from "$lib/components/Nav.svelte";
+  import { getAuth, logged, login, logout, notification } from "$lib/store";
+  import { ProgressBar } from "@skeletonlabs/skeleton";
+  import Fa from "svelte-fa";
+  import { faCircle } from "@fortawesome/free-solid-svg-icons";
+  import Notification from "$lib/components/Notification.svelte";
 
-	let items;
-	const itemRefreshTime = env.PUBLIC_REFRESH_INTERVAL; // in minutes
+  let items;
+  const itemRefreshTime = env.PUBLIC_REFRESH_INTERVAL; // in minutes
 
-	let saveItems = true;
-	const doNotsync = false;
+  let saveItems = true;
+  const doNotsync = true;
 
-	let syncing = false;
-	let firstUpdate = true;
+  let syncing = false;
+  let firstUpdate = true;
 
-	$: {
-		// check if loclStorage is empty
-		if (items) {
-			firstUpdate = false;
-		}
-	}
+  $: {
+    // check if loclStorage is empty
+    if (items) {
+      firstUpdate = false;
+    }
+  }
 
-	let password = '';
+  let password = "";
 
-	onMount(() => {
-		getAuth();
-		document.title = 'Emprunt';
-		if (doNotsync) {
-			console.log('doNotsync');
-			items = JSON.parse(window.localStorage.getItem('items'));
-		} else {
-			setLocalStorage();
-		}
-	});
+  onMount(() => {
+    getAuth();
+    document.title = "Emprunt";
+    if (doNotsync) {
+      console.log("doNotsync");
+      items = JSON.parse(window.localStorage.getItem("items"));
+    } else {
+      setLocalStorage();
+    }
+  });
 
-	async function fetchItems() {
-		syncing = true;
-		const response = await fetch('/api/items');
-		const data = await response.json();
-		console.log('data', data);
-		if (data.error) {
-			console.log('error', data.error);
-			$notification = ({
-				type: 'variant-filled-warning',
-				body: data.error,
-				show: true,
-			});
-		}
-		else {
-			syncing = false;
-			return data;
-		}
+  async function fetchItems() {
+    syncing = true;
+    const response = await fetch("/api/items");
+    const data = await response.json();
+    console.log("data", data);
+    if (data.error) {
+      console.log("error", data.error);
+      $notification = {
+        type: "variant-filled-warning",
+        body: data.error,
+        show: true,
+      };
+    } else {
+      syncing = false;
+      return data;
+    }
+  }
 
-	}
+  function setLocalStorage() {
+    const itemsLocalStorage = window.localStorage.getItem("items");
+    setTimeout(async () => {
+      if (itemsLocalStorage && saveItems == true) {
+        // check if items.lastUpdate is older than 5 minutes
+        items = JSON.parse(itemsLocalStorage);
+        const now = new Date();
+        const itemslastUpdate = new Date(items.lastUpdate);
+        const diff = now.getTime() - itemslastUpdate.getTime();
+        const diffMinutes = Math.round(((diff % 86400000) % 3600000) / 60000);
+        console.log(diffMinutes);
+        saveItems = false;
+        if (diffMinutes > itemRefreshTime) {
+          console.log(
+            "items in localStorage but older than ",
+            itemRefreshTime,
+            " minutes"
+          );
+          // fetch api/items and save it in localStorage with items.data and itemslastUpdate
+          // fetch('http://localhost:3000/api/items')
 
-	function setLocalStorage() {
-		const itemsLocalStorage = window.localStorage.getItem('items');
-		setTimeout(async () => {
-			if (itemsLocalStorage && saveItems == true) {
-				// check if items.lastUpdate is older than 5 minutes
-				items = JSON.parse(itemsLocalStorage);
-				const now = new Date();
-				const itemslastUpdate = new Date(items.lastUpdate);
-				const diff = now.getTime() - itemslastUpdate.getTime();
-				const diffMinutes = Math.round(((diff % 86400000) % 3600000) / 60000);
-				console.log(diffMinutes);
-				saveItems = false;
-				if (diffMinutes > itemRefreshTime) {
-					console.log('items in localStorage but older than ', itemRefreshTime, ' minutes');
-					// fetch api/items and save it in localStorage with items.data and itemslastUpdate
-					// fetch('http://localhost:3000/api/items')
+          const data = await fetchItems();
+          items.data = data;
+          items.lastUpdate = new Date().toString();
+          window.localStorage.setItem("items", JSON.stringify(items));
+        } else {
+          console.log(
+            "items in localStorage and younger than ",
+            itemRefreshTime,
+            " minutes"
+          );
+        }
+      } else {
+        console.log("no items in localStorage");
+        // fetch api/items and save it in localStorage with items.data and itemslastUpdate
 
-					const data = await fetchItems();
-					items.data = data;
-					items.lastUpdate = new Date().toString();
-					window.localStorage.setItem('items', JSON.stringify(items));
-				} else {
-					console.log('items in localStorage and younger than ', itemRefreshTime, ' minutes');
-				}
-			} else {
-				console.log('no items in localStorage');
-				// fetch api/items and save it in localStorage with items.data and itemslastUpdate
-
-				const data = await fetchItems();
-				items = {
-					data: data,
-					lastUpdate: new Date().toString()
-				};
-				window.localStorage.setItem('items', JSON.stringify(items));
-			}
-			setLocalStorage();
-		}, 5000);
-	}
-
+        const data = await fetchItems();
+        items = {
+          data: data,
+          lastUpdate: new Date().toString(),
+        };
+        window.localStorage.setItem("items", JSON.stringify(items));
+      }
+      setLocalStorage();
+    }, 5000);
+  }
 </script>
+
 <div class="hidden">
-	<LightSwitch />
+  <LightSwitch />
 </div>
-<div class="flex flex-col mt-12">
-	{#if !$logged}
-		<input type="text" placeholder="mot de passe" bind:value={password} />
-		<button on:click={() => login(password)}>login</button>
-	{/if}
-	{#if $logged}
-		<div class="flex justify-between p-4 fixed top-0 w-full ">
-			<button on:click={() => logout()}>logout</button> 
-			<div class="flex items-center">
-				<p class="mr-2 ">{syncing ? "synchronisation en cours" : "aucune synchronisation"}</p>
-				<div class="{syncing ? "text-orange-500 animate-pulse" : "text-green-500"}"><Fa icon={faCircle}/></div>
-			</div>	
-		</div>
-		{#if firstUpdate}
-			<div class="relative">
-				<h1>Mise en cache des items de la techshop</h1>
-				<p>La première fois, cela peut prendre plusieurs minutes</p>
-				<ProgressBar label="Progress Bar" value={undefined}/>
-			</div>
-		{/if}
-		<div class="{firstUpdate ? "opacity-5" : ""}">
-			<Notification />
-			<slot/>
-			<Nav/>
-		</div>
-	{/if}
-	
+<div class="flex flex-col mt-10">
+  {#if !$logged}
+    <input type="text" placeholder="mot de passe" bind:value={password} />
+    <button on:click={() => login(password)}>login</button>
+  {/if}
+  {#if $logged}
+    <div class="flex justify-between p-4 fixed top-0 w-full bg-surface-900">
+      <button on:click={() => logout()}>logout</button>
+      <div class="flex items-center">
+        <p class="mr-2">
+          {syncing ? "synchronisation en cours" : "aucune synchronisation"}
+        </p>
+        <div
+          class={syncing ? "text-orange-500 animate-pulse" : "text-green-500"}
+        >
+          <Fa icon={faCircle} />
+        </div>
+      </div>
+    </div>
+    {#if firstUpdate}
+      <div class="relative">
+        <h1>Mise en cache des items de la techshop</h1>
+        <p>La première fois, cela peut prendre plusieurs minutes</p>
+        <ProgressBar label="Progress Bar" value={undefined} />
+      </div>
+    {/if}
+    <div class={firstUpdate ? "opacity-5" : ""}>
+      <Notification />
+      <slot />
+      <Nav />
+    </div>
+  {/if}
 </div>
